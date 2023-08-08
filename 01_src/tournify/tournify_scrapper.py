5694ce9d3ce9a021e2b57c2c267997a8adb03db3
Jesper van Beemdelust <jespervanbeemdelust@hotmail.com>
2023-08-08 21:23:09 +0200

    Updated the header

---
+++ 01_src/tournify/tournify_scrapper.py
@@ -0,0 +1,30 @@
+"""
+[Script Name]: tournify_scrapper
+
+[Description]
+This script scrapes data from the tournify website. It's made to be for the Canoe Polo tournaments, but I suppose it could be used for other tournaments too.
+
+[Usage]
+1. You just need to provide an input URL. The script will output some CSV files with data.
+
+[Author]: Jesper van Beemdelust
+
+[Notes]
+- [Any additional notes or information you want to include.]
+"""
+
+# Import necessary libraries
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+# ... (add more imports as needed)
+
+def main():
+    """
+    Main function to execute the script.
+    """
+    # Your code here
+
+if __name__ == "__main__":
+    main()
