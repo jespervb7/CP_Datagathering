12d3e1342a643443525cf53cda7c363142e2b9e8
Jesper van Beemdelust <jespervanbeemdelust@hotmail.com>
2023-08-10 15:44:00 +0200

    HTML function is fixed

--- 01_src/tournify/tournify_scrapper.py
+++ 01_src/tournify/tournify_scrapper.py
@@ -16,6 +16,7 @@ This script scrapes data from the tournify website. It's made to be for the Cano
 # Import necessary libraries
 
 from bs4 import BeautifulSoup
+import sys
 import requests
 import pandas as pd
 import uuid
@@ -43,15 +44,23 @@ def get_url() -> str:
         return user_url
     
 def get_html(base_url: str) -> str:
-    html_str = requests.get(base_url).text
-    # conncetion error?
-    # url not found - Call get url again.
-    return html_str
+
+    try:
+        html_str = requests.get(base_url).text
+        return html_str
+    except requests.exceptions.RequestException as e:
+        # Handles any network-related exceptions
+        print(f"An error occurred while fetching the HTML: {e} from {base_url}")
+        print("Check your network settings and try again")
+        sys.exit(1)
+
 
 def parse_html(base_url: str) -> BeautifulSoup:
     html_str = get_html(base_url)
 
     soup = BeautifulSoup(html_str, "html.parser")
+
+    return soup
 
 def main():
     """
